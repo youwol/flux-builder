dd556624688460e831aa4f47c6dd9cf97ade10e9
greinisch <greinisch@youwol.com>
2021-09-17 16:54:16 +0200

    :art: attributes panel => cleaning

--- src/app/builder-editor/panel-module-attributes.ts
+++ src/app/builder-editor/panel-module-attributes.ts
@@ -6,6 +6,7 @@ import { render, VirtualDOM } from '@youwol/flux-view'
 import { AppObservables, AppStore } from './builder-state/index';
 import { ModuleSettingsState, ModuleSettingsView } from './views/module-settings.view';
 import { ConnectionSettingsState, ConnectionSettingsView } from './views/connection-settings.view';
+import { Module } from 'node:module';
 
 
 function moduleControls(mdle: ModuleFlux, appStore: AppStore): VirtualDOM {
@@ -23,17 +24,27 @@ function connectionControls(connection: Connection, appStore: AppStore): Virtual
 export function createAttributesPanel(appStore: AppStore, appObservables: AppObservables) {
 
     merge(appObservables.moduleSelected$, appObservables.connectionSelected$).subscribe(
-        (d: any) => {
-
-            document.getElementById("attributes-panel").innerHTML = ""
-            let panelDiv = document.getElementById("attributes-panel") as HTMLDivElement
-            let virtualDOM = d.moduleId ?
+        (d: ModuleFlux | Connection) => {
+            
+            let virtualDOM = {
+                id: "attribute-panel",
+                class: "panel-builder text-left w-100 h-100 position-relative d-flex flex-column fv-text-primary",
+                style: { fontSize: "small", paddingTop: "0px" },
+                children:[
+                    d instanceof ModuleFlux 
+                    ? moduleControls(d as ModuleFlux, appStore) 
+                    : connectionControls(d as Connection, appStore)
+                ] 
+            }
+            document.getElementById("panels-container-builder").innerHTML = ""
+            let panelDiv = document.getElementById("panels-container-builder") as HTMLDivElement
+            /*let virtualDOM = d.moduleId ?
                 moduleControls(d as ModuleFlux, appStore) :
-                connectionControls(d as Connection, appStore)
+                connectionControls(d as Connection, appStore)*/
             panelDiv.appendChild(render(virtualDOM))
         }
     )
     appObservables.unselect$.subscribe((_) => {
-        document.getElementById("attributes-panel").innerHTML = ""
+        document.getElementById("panels-container-builder").innerHTML = ""
     })
 }
