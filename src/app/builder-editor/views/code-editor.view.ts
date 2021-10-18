424630e931992f9dbcbce6b9a89211598de4d81a
Julien DÉCHARNE <jdecharne@youwol.com>
2021-10-18 19:48:23 +0200

    :heavy_plus_sign: @type/codemirror

--- src/app/builder-editor/views/code-editor.view.ts
+++ src/app/builder-editor/views/code-editor.view.ts
@@ -1,112 +1,106 @@
-import { child$, VirtualDOM } from "@youwol/flux-view";
-import { BehaviorSubject, from } from "rxjs";
-import { mergeMap } from "rxjs/operators";
+/** @format */
 
+import { child$, VirtualDOM } from '@youwol/flux-view'
+import CodeMirror from 'codemirror'
+import { BehaviorSubject, from } from 'rxjs'
+import { mergeMap } from 'rxjs/operators'
 
-export namespace CodeEditorView{
+export namespace CodeEditorView {
+  export class State {
+    public readonly content$: BehaviorSubject<string>
 
+    constructor({ content$ }: { content$: BehaviorSubject<string> }) {
+      this.content$ = content$
+    }
+  }
 
-    export class State{
-        
-        public readonly content$ : BehaviorSubject<string>
+  type TOptions = {
+    containerClass?: string
+    containerStyle?: { [key: string]: string }
+  }
 
-        constructor({
-                content$ 
-            }:
-            {
-                content$: BehaviorSubject<string>
-            }
-        ){
-            this.content$ = content$
-        }
+  export class View implements VirtualDOM {
+    static defaultOptions = {
+      containerClass: 'h-100 w-100',
+      containerStyle: {},
+    }
+    public readonly state: State
+    public readonly class: string
+    public readonly style: { [key: string]: string }
+    public readonly children: Array<VirtualDOM>
+
+    constructor({
+      state,
+      editorConfiguration,
+      options,
+      ...rest
+    }: {
+      state: State
+      editorConfiguration: CodeMirror.EditorConfiguration
+      options?: TOptions
+    }) {
+      Object.assign(this, rest)
+      const styling: TOptions = {
+        ...View.defaultOptions,
+        ...(options ? options : {}),
+      }
+      this.state = state
+      this.class = styling.containerClass
+      this.style = styling.containerStyle
+      const configuration = {
+        ...{
+          value: state.content$.getValue(),
+          mode: 'javascript',
+          lineNumbers: true,
+          theme: 'blackboard',
+          extraKeys: {
+            Tab: (cm) => cm.replaceSelection('    ', 'end'),
+          },
+        },
+        ...(editorConfiguration || {}),
+      }
+
+      this.children = [
+        child$(this.fetchCodeMirror$(configuration.mode as string), () => {
+          return {
+            id: 'code-mirror-editor',
+            class: 'w-100 h-100',
+            connectedCallback: (elem) => {
+              const editor: CodeMirror.Editor = window['CodeMirror'](
+                elem as ParentNode,
+                configuration,
+              )
+              editor.on('changes', () => {
+                state.content$.next(editor.getValue())
+              })
+            },
+          }
+        }),
+      ]
     }
 
+    fetchCodeMirror$(mode: string) {
+      const cdn = window['@youwol/cdn-client']
 
-    type TOptions = {
-        containerClass?: string,
-        containerStyle?: {[key:string]: string},
+      const urlsMode = {
+        javascript: 'codemirror#5.52.0~mode/javascript.min.js',
+        python: 'codemirror#5.52.0~mode/python.min.js',
+        css: 'codemirror#5.52.0~mode/css.min.js',
+        xml: 'codemirror#5.52.0~mode/xml.min.js',
+        html: 'codemirror#5.52.0~mode/htmlmixed.min.js',
+      }
+      return from(
+        cdn.fetchBundles({ codemirror: { version: '5.52.0' } }, window),
+      ).pipe(
+        mergeMap(() => {
+          const urls = Array.isArray(urlsMode[mode])
+            ? urlsMode[mode]
+            : [urlsMode[mode]]
+
+          const promise = cdn.fetchJavascriptAddOn(urls, window)
+          return from(promise)
+        }),
+      )
     }
-
-    export class View implements VirtualDOM{
-
-        static defaultOptions  = {
-            containerClass: 'h-100 w-100',
-            containerStyle: {},
-        }
-        public readonly state: State
-        public readonly class: string
-        public readonly style: {[key:string]: string}
-        public readonly children: Array<VirtualDOM>
-
-        constructor({
-            state,
-            editorConfiguration,
-            options,
-            ...rest
-        }: {
-            state: State,
-            editorConfiguration: any,
-            options?: TOptions
-        }){
-            Object.assign(this, rest)
-            const styling : TOptions = {...View.defaultOptions, ...(options ? options : {}) }
-            this.state = state
-            this.class = styling.containerClass
-            this.style = styling.containerStyle
-            const configuration = {
-                ...{
-                    value: state.content$.getValue(),
-                    mode: 'javascript',
-                    lineNumbers: true,
-                    theme:'blackboard',
-                    extraKeys: {
-                        "Tab": (cm) => cm.replaceSelection("    " , "end")
-                       }
-                },
-                ...( editorConfiguration || {} )
-            }
-
-            this.children = [
-                child$( 
-                    fetchCodeMirror$(configuration.mode),
-                    () => {
-                        return {
-                            id: 'code-mirror-editor',
-                            class: 'w-100 h-100',
-                            connectedCallback: (elem) => {
-                                const editor = window['CodeMirror'](elem, configuration)
-                                editor.on("changes" , () => {
-                                    state.content$.next(editor.getValue())
-                                })
-                            }
-                        }
-                    }
-                )
-            ]
-        }
-    }
-
-    function fetchCodeMirror$(mode: string){
-        const cdn = window['@youwol/cdn-client']
-        
-        const urlsMode = {
-            "javascript": "codemirror#5.52.0~mode/javascript.min.js",
-            "python": "codemirror#5.52.0~mode/python.min.js",
-            "css":  "codemirror#5.52.0~mode/css.min.js",
-            "xml":  "codemirror#5.52.0~mode/xml.min.js",
-            "html":  "codemirror#5.52.0~mode/htmlmixed.min.js"                
-        }
-        return from(cdn.fetchBundles( {  codemirror: { version: '5.52.0' } },  window)
-        ).pipe(
-            mergeMap( () =>{
-                const urls = Array.isArray(urlsMode[mode]) ? urlsMode[mode] : [urlsMode[mode]]
-
-                const promise = cdn.fetchJavascriptAddOn(
-                    urls, 
-                    window
-                    )
-                return from(promise)
-            })
-        )
-    }
+  }
 }
