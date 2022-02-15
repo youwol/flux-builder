78b05612d5986a7aee1a77af355a0f1f57b47789
greinisch <greinisch@youwol.com>
2022-02-15 15:29:02 +0100

    🔨 update yw_pipeline; include target definition

--- .yw_pipeline/yw_pipeline.py
+++ .yw_pipeline/yw_pipeline.py
@@ -1,18 +1,8 @@
-from youwol.pipelines.pipeline_typescript_weback_npm import pipeline
 from youwol.environment.forward_declaration import YouwolEnvironment
 from youwol.environment.models import IPipelineFactory
-from youwol.environment.models_project import PipelineStep, Pipeline, Flow
+from youwol.environment.models_project import BrowserApp, Execution, FromAsset
+from youwol.pipelines.pipeline_typescript_weback_npm import pipeline, PipelineConfig
 from youwol_utils.context import Context
-
-
-class CreateTestEnv(PipelineStep):
-    id: str = 'create-test-env'
-    run: str = 'yarn create-test-env'
-
-
-class StartTestEnv(PipelineStep):
-    id: str = 'start-test-env'
-    run: str = 'yarn start-test-env'
 
 
 class PipelineFactory(IPipelineFactory):
@@ -20,21 +10,21 @@ class PipelineFactory(IPipelineFactory):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    async def get(self, env: YouwolEnvironment, ctx: Context):
-        base = pipeline()
-        flow = Flow(
-                name="prod",
-                dag=[
-                    "checks > init > sync-deps > build-prod > test > publish-local > publish-remote ",
-                    "create-test-env > start-test-env > test > test-coverage",
-                    "build-prod > doc > publish-local"
+    async def get(self, _env: YouwolEnvironment, context: Context):
+        config = PipelineConfig(
+            with_tags=["flux"],
+            target=BrowserApp(
+                icon={"class": "fas fa-tools"},
+                displayName="Flux builder",
+                execution=Execution(
+                    standalone=False,
+                    parametrized=[
+                        FromAsset(
+                            match={"kind": "flux-project"},
+                            parameters={"id": 'rawId'}
+                        )
                     ]
                 )
-        return Pipeline(**{
-                **base.dict(),
-                **{
-                    "steps": base.steps + [CreateTestEnv(), StartTestEnv()],
-                    "flows":  [flow] + [f for f in base.flows if f.name != 'prod']
-                }
-            }
+            )
         )
+        return await pipeline(config, context)
