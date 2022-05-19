2b2b1fac8097ccade02e0c9e49d4d765ce264c2a
greinisch <greinisch@youwol.com>
2022-05-19 15:32:07 +0200

    🎨 auto-format by es-lint

--- src/app/builder-editor/views/modal.view.ts
+++ src/app/builder-editor/views/modal.view.ts
@@ -1,66 +1,78 @@
-import { render, VirtualDOM } from "@youwol/flux-view"
-import { Button } from "@youwol/fv-button"
-import { Modal } from "@youwol/fv-group"
-import { merge, Observable, Subject } from "rxjs"
+/** @format */
 
+import { render, VirtualDOM } from '@youwol/flux-view'
+import { Button } from '@youwol/fv-button'
+import { Modal } from '@youwol/fv-group'
+import { merge, Observable, Subject } from 'rxjs'
 
-export namespace ModalView{
-        
+export namespace ModalView {
     export function popup({
         view,
         style,
         ok$,
-        options
+        options,
     }: {
-        view: VirtualDOM,
-        ok$?: Subject<MouseEvent>,
-        style?: {[key:string]: string},
-        options?: {displayOk: boolean, displayCancel: boolean}
-    }
-    ): Observable<MouseEvent>{
-
-        options = options || {displayOk:true, displayCancel: true}
+        view: VirtualDOM
+        ok$?: Subject<MouseEvent>
+        style?: { [key: string]: string }
+        options?: { displayOk: boolean; displayCancel: boolean }
+    }): Observable<MouseEvent> {
+        options = options || { displayOk: true, displayCancel: true }
 
         const okBttn = new Button.View({
             state: new Button.State(),
-            contentView: () => ({ innerText: 'Ok'}),
-            class: "fv-btn fv-btn-primary fv-bg-focus mr-2"
+            contentView: () => ({ innerText: 'Ok' }),
+            class: 'fv-btn fv-btn-primary fv-bg-focus mr-2',
         } as any)
 
         const cancelBttn = Button.simpleTextButton('Cancel')
-        
+
         const modalState = new Modal.State(ok$)
         const modalDiv = render(
             new Modal.View({
                 state: modalState,
                 contentView: () => {
                     return {
-                        class:'border rounded fv-text-primary fv-bg-background d-flex flex-column',
-                        style: style ? style : { height:'50vh', width:'50vw', 'max-width':'1500px'},
-                        children:[
+                        class: 'border rounded fv-text-primary fv-bg-background d-flex flex-column',
+                        style: style
+                            ? style
+                            : {
+                                  height: '50vh',
+                                  width: '50vw',
+                                  'max-width': '1500px',
+                              },
+                        children: [
                             view,
                             {
-                                class:'d-flex p-2',
-                                children:[
+                                class: 'd-flex p-2',
+                                children: [
                                     options.displayOk ? okBttn : undefined,
-                                    options.displayCancel ? cancelBttn : undefined
-                                ].filter(d => d)
-                            }
-                        ]
+                                    options.displayCancel
+                                        ? cancelBttn
+                                        : undefined,
+                                ].filter((d) => d),
+                            },
+                        ],
                     }
                 },
                 connectedCallback: (elem) => {
                     const subs = [
-                        okBttn.state.click$.subscribe( () => modalState.ok$.next()),
-                        cancelBttn.state.click$.subscribe( () => modalState.cancel$.next()),
-                        merge(modalState.cancel$, modalState.ok$ ).subscribe( () => modalDiv.remove() )
+                        okBttn.state.click$.subscribe(() =>
+                            modalState.ok$.next(),
+                        ),
+                        cancelBttn.state.click$.subscribe(() =>
+                            modalState.cancel$.next(),
+                        ),
+                        merge(modalState.cancel$, modalState.ok$).subscribe(
+                            () => modalDiv.remove(),
+                        ),
                     ]
                     elem.subscriptions = [...elem.subscriptions, ...subs]
-                }
-            }as any)
+                },
+            } as any),
         )
 
-        document.querySelector("body").appendChild(modalDiv)
+        document.querySelector('body').appendChild(modalDiv)
         return modalState.ok$
     }
 }
