e1999714a36c88c96f7ab792466c919e16f15c3c
greinisch <greinisch@youwol.com>
2022-07-25 10:40:32 +0200

    👽️ assets-browser.client.ts => use new cdn-client API

--- src/app/clients/assets-browser.client.ts
+++ src/app/clients/assets-browser.client.ts
@@ -3,7 +3,7 @@
 import { createObservableFromFetch } from '@youwol/flux-core'
 import { from, Observable, of } from 'rxjs'
 import { map, mergeMap, tap } from 'rxjs/operators'
-import { install } from '@youwol/cdn-client'
+import { installModules } from '@youwol/cdn-client'
 import { AppStore } from '../builder-editor/builder-state'
 
 export interface AssetResp {
@@ -156,15 +156,13 @@ export class AssetsBrowserClient {
                     ...{ [targetLibrary.name]: targetLibrary.versions[0] },
                 } as { [key: string]: string }
 
-                const fetchPromise = install(
-                    {
-                        modules: Object.entries(libraries).map(([k, v]) => ({
-                            name: k,
-                            version: v,
-                        })),
-                    },
-                    { executingWindow: window },
-                )
+                const fetchPromise = installModules({
+                    modules: Object.entries(libraries).map(([k, v]) => ({
+                        name: k,
+                        version: v,
+                    })),
+                    executingWindow: window,
+                })
 
                 return from(fetchPromise).pipe(
                     map((loadingGraph) => {
